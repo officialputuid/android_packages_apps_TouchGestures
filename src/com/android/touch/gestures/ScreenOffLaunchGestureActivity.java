e35b3b2bf22ca06c8eb7b87114d7b08142b8c851
Bruno Martins <bgcngm@gmail.com>
2022-12-06 17:55:45 +0000

    TouchGestures: Fix compilation for QPR1 merge
    Change-Id: I71ad7db85592ca18e3f0f8155bba5a472bfadbaa
    Signed-off-by: officialputuid <officialputuid@hack.id>

--- src/com/android/touch/gestures/ScreenOffLaunchGestureActivity.java
+++ src/com/android/touch/gestures/ScreenOffLaunchGestureActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.os.Handler;
 
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
-import com.android.settingslib.collapsingtoolbar.R;
+import com.android.settingslib.widget.R;
 
 public class ScreenOffLaunchGestureActivity extends CollapsingToolbarBaseActivity {
 
